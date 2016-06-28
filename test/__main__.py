30fb6694c89cd091504f2a4723aff3d352e8866a
iury <satriani-16@hotmail.com>
2016-06-28 12:22:37 -0300

    test build[4]

--- test/__main__.py
+++ test/__main__.py
@@ -7,10 +7,13 @@ from __future__ import (
 )
 
 import sys
+import os
 import unittest
 
 
 def main():
+    project_path = os.path.split(os.path.dirname(__file__))
+    sys.path.append(project_path[0])
     tests = unittest.TestLoader().discover("test", "*tests.py")
     result = unittest.TextTestRunner().run(tests)
 
